fb6d093feed8c35bd140f6d9f4fd077a29728243
xsummersx <605369747@qq.com>
2022-02-23 16:15:40 +0800

    push

--- src/components/eduSchool/Map.vue
+++ src/components/eduSchool/Map.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 吴涛
  * @Date: 2021-11-30 14:27:26
- * @LastEditTime: 2022-02-23 11:33:23
+ * @LastEditTime: 2022-02-23 16:15:30
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: 教育局、学校校长=》地图，图0
@@ -562,8 +562,8 @@ export default {
   position: absolute;
   width: 100px;
   height: 20px;
-  right: 30px;
-  top: 28px;
+  right: 8px;
+  top: 10px;
   cursor: pointer;
   .text {
     line-height: 20px;
