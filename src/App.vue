23a5344c43690e5a66d6d9a89a4036ecda2509d8
xsummersx <605369747@qq.com>
2022-03-28 11:35:23 +0800

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-03-21 13:46:03
+ * @LastEditTime: 2022-03-28 11:32:58
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \znbk_rzzd_dx_web_new\src\App.vue
+-->
 <script setup>
   const router = useRouter();
   const toPage = (item) => {
